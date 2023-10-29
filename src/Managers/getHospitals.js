642f93a0f5514715dea259c0b21fcaf192c4a9e3
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-10-29 05:32:11 -0700

    Response object in hospitalComparator contains image

--- src/Managers/getHospitals.js
+++ src/Managers/getHospitals.js
@@ -1,79 +1,95 @@
-const axios = require('axios');
-const api_key = "AIzaSyB29yN9o9YQdUNc8BcepzN5RmjD4e3dBV4";  // Please replace with your actual API key
+const axios = require("axios");
+const api_key = "AIzaSyB29yN9o9YQdUNc8BcepzN5RmjD4e3dBV4"; // Please replace with your actual API key
 
 // Function to geocode address using Google Maps Geocoding API
 async function geocodeAddress(address) {
   try {
-    const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-      params: {
-        address: address,
-        key: api_key
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/geocode/json",
+      {
+        params: {
+          address: address,
+          key: api_key,
+        },
       }
-    });
+    );
     const location = response.data.results[0].geometry.location;
     return location;
   } catch (error) {
-    console.error('Error geocoding address: ', error);
+    console.error("Error geocoding address: ", error);
     throw error;
   }
 }
 
 // Function to get nearby hospitals using Google Places API
 async function getNearbyHospitals(latitude, longitude) {
-    try {
-      const response = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+  try {
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+      {
         params: {
           location: `${latitude},${longitude}`,
-          radius: 48280,  // 30 miles in meters
-          keyword: 'hospital',
-          key: api_key
-        }
-      });
-      const hospitals = response.data.results;
-      return hospitals;
-    } catch (error) {
-      console.error('Error getting nearby hospitals: ', error);
-      throw error;
-    }
+          radius: 48280, // 30 miles in meters
+          keyword: "hospital",
+          key: api_key,
+        },
+      }
+    );
+    const hospitals = response.data.results.map((hospital) => ({
+      ...hospital,
+      photoUrl:
+        hospital.photos && hospital.photos.length > 0
+          ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=1800&photoreference=${hospital.photos[0].photo_reference}&key=${api_key}`
+          : null,
+    }));
+    return hospitals;
+  } catch (error) {
+    console.error("Error getting nearby hospitals: ", error);
+    throw error;
   }
+}
 
 // Main function to get a list of hospitals within 30 miles of a given address
 async function getHospitals(location) {
-    try {
-      const hospitals = await getNearbyHospitals(location.lat, location.lng);
-      return hospitals;
-    } catch (error) {
-      console.error('Error: ', error);
-      throw error;
-    }
+  try {
+    const hospitals = await getNearbyHospitals(location.lat, location.lng);
+    return hospitals;
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
   }
+}
 
 // Function to get distance and time from user location to hospitals
 async function getDistanceAndTime(origin, destinations, hospitalDetails) {
-    const formattedUserLocation = `${origin.lat},${origin.lng}`;  // Adjusted this line to use the origin parameter
-    const formattedDestinations = destinations.join('|');  // destinations is already an array of addresses
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(formattedUserLocation)}&destinations=${encodeURIComponent(formattedDestinations)}&units=imperial&key=${api_key}`;
-    try {
-        const response = await axios.get(url);
-        const { rows } = response.data;
-        const elements = rows[0].elements;
+  const formattedUserLocation = `${origin.lat},${origin.lng}`; // Adjusted this line to use the origin parameter
+  const formattedDestinations = destinations.join("|"); // destinations is already an array of addresses
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(
+    formattedUserLocation
+  )}&destinations=${encodeURIComponent(
+    formattedDestinations
+  )}&units=imperial&key=${api_key}`;
+  try {
+    const response = await axios.get(url);
+    const { rows } = response.data;
+    const elements = rows[0].elements;
 
-        return destinations.map((address, index) => {
-            const { distance, duration } = elements[index];
-            const hospital = hospitalDetails[index];
-            return {
-                hospital_name: hospital.name,
-                hospital_address: address,
-                latitude: hospital.latitude,
-                longitude: hospital.longitude,
-                distanceFromAddr: distance.text,
-                travelTime: duration.text
-            };
-        });
-    } catch (error) {
-        console.error('Error getting distance and time: ', error);
-    }
+    return destinations.map((address, index) => {
+      const { distance, duration } = elements[index];
+      const hospital = hospitalDetails[index];
+      return {
+        hospital_name: hospital.name,
+        hospital_address: address,
+        latitude: hospital.latitude,
+        longitude: hospital.longitude,
+        distanceFromAddr: distance.text,
+        travelTime: duration.text,
+        photoUrl: hospital.photoUrl
+      };
+    });
+  } catch (error) {
+    console.error("Error getting distance and time: ", error);
+  }
 }
 
-
-module.exports = {getNearbyHospitals, getDistanceAndTime,  getHospitals };
+module.exports = { getNearbyHospitals, getDistanceAndTime, getHospitals };
--- src/Managers/hospitalComparator.js
+++ src/Managers/hospitalComparator.js
@@ -1,98 +1,118 @@
-const { getDocs, collection, query, where } = require('firebase/firestore');
+// const log = require('why-is-node-running') // should be your first require
+// const net = require('net')
+
+const { getDocs, collection, query, where } = require("firebase/firestore");
 const { db } = require("./Config.js");
-const { getHospitals, getDistanceAndTime, getNearbyHospitals } = require('./getHospitals.js'); // Import getHospitals from getHospitals.js
-const geohash = require('ngeohash');  // Ensure you have the ngeohash package installed
+const {
+  getHospitals,
+  getDistanceAndTime,
+} = require("./getHospitals.js"); // Import getHospitals from getHospitals.js
+const geohash = require("ngeohash"); // Ensure you have the ngeohash package installed
 
 async function getHospitalDetailsAndDistances(origin, hospitals) {
-    const hospitalDetails = hospitals.map(hospital => ({
-        name: hospital.name,
-        address: hospital.vicinity,
-        latitude: hospital.geometry.location.lat,
-        longitude: hospital.geometry.location.lng
-    }));
-    const hospitalAddresses = hospitals.map(hospital => hospital.vicinity);
-    return await getDistanceAndTime(origin, hospitalAddresses, hospitalDetails);
+  const hospitalDetails = hospitals.map((hospital) => ({
+    name: hospital.name,
+    address: hospital.vicinity,
+    latitude: hospital.geometry.location.lat,
+    longitude: hospital.geometry.location.lng,
+    photoUrl: hospital.photoUrl,
+  }));
+  // console.log("first")
+  const hospitalAddresses = hospitals.map((hospital) => hospital.vicinity);
+  // console.log("hospital details", hospitalDetails);
+  return await getDistanceAndTime(origin, hospitalAddresses, hospitalDetails);
 }
 
-
 async function findIntersection(firebaseHospitals, searchHospitals) {
-    const intersectedHospitals = [];
-    const seenHospitalIds = new Set();
-    for (let searchHospital of searchHospitals) {
-      for (let firebaseHospital of firebaseHospitals) {
-        const distance = Math.hypot(
-          firebaseHospital.geoLocation.lat - searchHospital.latitude,
-          firebaseHospital.geoLocation.lng - searchHospital.longitude
-        );
-  
-        const isSameHospital = distance < 0.0002844; // distance in degrees for approx 1000 ft
-        if (isSameHospital) {
-          // console.log("SAME ENCOUNTER", searchHospital);
-          intersectedHospitals.push({
-            ...firebaseHospital,
-            distanceFromAddr: searchHospital.distanceFromAddr,
-            travelTime: searchHospital.travelTime
+  const intersectedHospitals = [];
+  for (let searchHospital of searchHospitals) {
+    for (let firebaseHospital of firebaseHospitals) {
+      const distance = Math.hypot(
+        firebaseHospital.geoLocation.lat - searchHospital.latitude,
+        firebaseHospital.geoLocation.lng - searchHospital.longitude
+      );
+
+      const isSameHospital = distance < 0.0002844; // distance in degrees for approx 1000 ft
+      if (isSameHospital) {
+        // console.log("SAME ENCOUNTER", searchHospital);
+        intersectedHospitals.push({
+          ...firebaseHospital,
+          distanceFromAddr: searchHospital.distanceFromAddr,
+          travelTime: searchHospital.travelTime,
+          photoUrl: searchHospital.photoUrl,
         });
-       }
       }
     }
-  
-    return intersectedHospitals.filter((item, 
-      index) => intersectedHospitals.indexOf(item) === index);
   }
-  
+
+  return intersectedHospitals.filter(
+    (item, index) => intersectedHospitals.indexOf(item) === index
+  );
+}
 
 function getBoundingBox(latitude, longitude, distanceInKm) {
-    const earthRadius = 6371;  // Earth radius in km
-    const deltaLat = (distanceInKm / earthRadius) * (180 / Math.PI);
-    const deltaLng = (distanceInKm / (earthRadius * Math.cos((Math.PI / 180) * latitude))) * (180 / Math.PI);
+  const earthRadius = 6371; // Earth radius in km
+  const deltaLat = (distanceInKm / earthRadius) * (180 / Math.PI);
+  const deltaLng =
+    (distanceInKm / (earthRadius * Math.cos((Math.PI / 180) * latitude))) *
+    (180 / Math.PI);
 
-    const minLat = latitude - deltaLat;
-    const maxLat = latitude + deltaLat;
-    const minLng = longitude - deltaLng;
-    const maxLng = longitude + deltaLng;
+  const minLat = latitude - deltaLat;
+  const maxLat = latitude + deltaLat;
+  const minLng = longitude - deltaLng;
+  const maxLng = longitude + deltaLng;
 
-    const minGeohash = geohash.encode(minLat, minLng);
-    const maxGeohash = geohash.encode(maxLat, maxLng);
+  const minGeohash = geohash.encode(minLat, minLng);
+  const maxGeohash = geohash.encode(maxLat, maxLng);
 
-    return {
-        minGeohash,
-        maxGeohash
-    };
+  return {
+    minGeohash,
+    maxGeohash,
+  };
 }
 
 async function getIntersection(origin) {
   // Step 1: Fetch hospitals from Firebase within 50km of the origin
-  const hospitalCollection = collection(db, 'Hospital');
+  const hospitalCollection = collection(db, "Hospital");
   const boundingBox = getBoundingBox(origin.lat, origin.lng, 50);
   const nearbyQuery = query(
-      hospitalCollection,
-      where('geoHash', '>=', boundingBox.minGeohash),
-      where('geoHash', '<=', boundingBox.maxGeohash)
+    hospitalCollection,
+    where("geoHash", ">=", boundingBox.minGeohash),
+    where("geoHash", "<=", boundingBox.maxGeohash)
   );
 
   const firebaseHospitalsSnap = await getDocs(nearbyQuery);
-  const firebaseHospitals = firebaseHospitalsSnap.docs.map(doc => doc.data());
+  const firebaseHospitals = firebaseHospitalsSnap.docs.map((doc) => doc.data());
   // console.log("Firebase hospitals that fall under the query: ", firebaseHospitals);
   // Step 2: Fetch hospitals from the Google Places API
-  const searchHospitals = await getHospitals(origin);  // Assuming getHospitals is defined
-  const detailsAndDistances = await getHospitalDetailsAndDistances(origin, searchHospitals);
-  // console.log("Search Results from the origin", searchHospitals);
+  const searchHospitals = await getHospitals(origin); // Assuming getHospitals is defined
+  const detailsAndDistances = await getHospitalDetailsAndDistances(
+    origin,
+    searchHospitals
+  );
+  // console.log("Search Results from the origin", detailsAndDistances);
   // Step 3: Find the intersection of the two sets
-  const intersectedHospitals = await findIntersection(firebaseHospitals, detailsAndDistances);
+
+  const intersectedHospitals = await findIntersection(
+    firebaseHospitals,
+    detailsAndDistances
+  );
 
   return intersectedHospitals;
 }
 
 const origin = {
-    lat: 37.7895567,
-    lng: -122.416863
+  lat: 37.7895567,
+  lng: -122.416863,
 };
 
 getIntersection(origin)
-    .then(intersectedHospitals => {
-        console.log(intersectedHospitals);
-    })
-    .catch(error => console.error(error));
+  .then((intersectedHospitals) => {
+    console.log(intersectedHospitals);
+  }).catch((error) => console.error(error));
 
 module.exports = { getIntersection };
+
+// setTimeout(function () {
+//   log() // logs out active handles that are keeping node running
+// }, 1000)
