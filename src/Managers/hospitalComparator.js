fa2fa80faa48b93a097256a096cd2e46f41cb1c2
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-10-28 22:22:03 -0700

    fix please

--- src/Managers/hospitalComparator.js
+++ src/Managers/hospitalComparator.js
@@ -1,6 +1,7 @@
-const { getDocs, query, where } = require('firebase/firestore');
-const { db } = require("../Components/FireBase/Config.js");
+const { getDocs, collection, query, where } = require('firebase/firestore');
+const { db } = require("./Config.js");
 const { getHospitals } = require('./getHospitals.js'); // Import getHospitals from getHospitals.js
+const geohash = require('ngeohash');  // Ensure you have the ngeohash package installed
 
 async function getHospitalDetailsAndDistances(origin, hospitals) {
     const hospitalDetails = hospitals.map(hospital => ({
@@ -34,16 +35,32 @@ async function findIntersection(firebaseHospitals, searchHospitals) {
     return intersectedHospitals;
   }
   
+
+function getBoundingBox(latitude, longitude, distanceInKm) {
+    const earthRadius = 6371;  // Earth radius in km
+    const deltaLat = (distanceInKm / earthRadius) * (180 / Math.PI);
+    const deltaLng = (distanceInKm / (earthRadius * Math.cos((Math.PI / 180) * latitude))) * (180 / Math.PI);
+
+    const minLat = latitude - deltaLat;
+    const maxLat = latitude + deltaLat;
+    const minLng = longitude - deltaLng;
+    const maxLng = longitude + deltaLng;
+
+    const minGeohash = geohash.encode(minLat, minLng);
+    const maxGeohash = geohash.encode(maxLat, maxLng);
+
+    return {
+        minGeohash,
+        maxGeohash
+    };
+}
+
 async function getIntersection(origin) {
   // Step 1: Fetch hospitals from Firebase within 50km of the origin
-  const geofirestore = require('geofirestore');
-  const GeoFirestore = geofirestore.initializeApp(db);  
-    const hospitalCollection = GeoFirestore.collection('Hospital');
-    const nearbyQuery = hospitalCollection.near({
-        center: new geofirestore.GeoPoint(origin.lat, origin.lng),
-        radius: 50
-    });
-    
+   const hospitalCollection = collection(db, 'Hospital');
+  const boundingBox = getBoundingBox(origin.lat, origin.lng, 50);  // Assume getBoundingBox is a function to get the bounding box
+  const nearbyQuery = hospitalCollection.where('geohash', '>=', boundingBox.minGeohash).where('geohash', '<=', boundingBox.maxGeohash);
+  
   const firebaseHospitalsSnap = await getDocs(nearbyQuery);
   const firebaseHospitals = firebaseHospitalsSnap.docs.map(doc => doc.data());
 
